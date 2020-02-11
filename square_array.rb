5812549c1fe80dda9e4e2438f5b7c54545a3a381
Kyle Talmage <ktalmage85@gmail.com>
2020-02-11 21:43:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(array)
-  array.each {|number| number**2,""}
+def square_array(array,exponent)
+  array.each {|number| number*exponent}
 end
 
 array = [1,2,3]
 
-puts square_array(array)
+puts square_array(array,2)
